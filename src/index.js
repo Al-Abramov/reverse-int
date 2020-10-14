6961255405544b4f04c614f4607f720a5f7da3d1
Al-Abramov <x3m.f@mail.ru>
2020-10-14 23:57:54 +0400

    feat: add implement function for task Reverse Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
-}
+  if(n < 0){
+      let res = n.toString().split("").reverse().join("").slice(0,-1);
+      return Number(res);
+  }else{
+      let res = n.toString().split('').reverse().join('');
+      return Number(res);
+  }
+  }
