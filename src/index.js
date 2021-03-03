482cc106f721449e75fbd5a6c6453ea8d9e29a2e
Alex <x3m.f@mail.ru>
2021-03-03 14:13:21 +0400

    feat: add implement function for task reverse

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 module.exports = function reverse (n) {
-  if(n < 0){
-      let res = n.toString().split("").reverse().join("").slice(0,-1);
-      return Number(res);
-  }else{
-      let res = n.toString().split('').reverse().join('');
-      return Number(res);
-  }
+        let res = Math.abs(n).toString().split('').reverse().join('');
+        return Number(res);
   }
